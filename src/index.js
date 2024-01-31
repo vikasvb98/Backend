a2e9da9a352f39153afb07226505cc8ebc40b65b
vikasvb98 <vikashkhandelwal1998@gmail.com>
2024-01-31 14:18:56 +0530

    models and utils

--- src/index.js
+++ src/index.js
@@ -11,7 +11,14 @@ dotenv.config({
 })
 
 connectDB()
-
+ .then( () => {
+    app.listen(process.env.PORT || 3000, () => {
+        console.log(`Server is running at port ${process.env.PORT}`)
+    })
+ })
+ .catch( (error) => {
+    console.log("MONGODB connection failed", error)
+ })
 
 
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,30 @@
+const asyncHandler = (requestHandler) => {
+    (res,req,next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
+
+export {asyncHandler}
+
+
+
+
+
+
+
+
+
+
+
+
+// const asyncHandler = (fn) => async (req,res,next) => {
+//     try {
+//         await fn(req,res,next)
+
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
